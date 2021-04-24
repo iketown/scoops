de1e7ca41dda982a941048607867a9377c6fe40a
ike76 <ike76@me.com>
2021-04-24 13:42:21 -0500

    move homegrid

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,10 @@ import Buckets from "components/Buckets";
 import { AnimatePresence } from "framer-motion";
 import { nanoid } from "nanoid";
 import { useCallback, useState } from "react";
-import styled from "styled-components";
+import { HomeGrid } from "utils/HomeGrid";
 
 import ConeDisplay from "./components/ConeDisplay";
 import History from "./components/History";
-
-const HomeGrid = styled.div`
-  display: flex;
-  margin: 2rem;
-  overflow: scroll;
-  max-width: 100vw;
-  background-image: url(images/stripedBG.svg);
-  background-size: 6rem;
-`;
 
 function App() {
   const [selectedConeId, setSelectedConeId] = useState<string>("");
---
+++ src/utils/HomeGrid.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HomeGrid = styled.div`
+  display: flex;
+  margin: 2rem;
+  overflow: scroll;
+  max-width: 100vw;
+  background-image: url(images/stripedBG.svg);
+  background-size: 6rem;
+`;
